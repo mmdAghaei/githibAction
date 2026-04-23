b843c1831c354ce8c67d7a0ae5fc49fddcaaf4a1
Mohammad M. Aghaei <164787029+mmdAghaei@users.noreply.github.com>
2026-04-23 14:19:17 +0330

    Update script.py

--- script.py
+++ script.py
@@ -1,17 +1,20 @@
-from aiobale import Client, Dispatcher
-import asyncio
-from aiobale.enums import ChatType
+# from aiobale import Client, Dispatcher
+# import asyncio
+# from aiobale.enums import ChatType
 
-async def send_personal_message():
-    dp = Dispatcher()
-    client = Client(dp,session_file="session")
-    async def open_gift_handler():
-        try:
-            await client.send_message(text=f"github action is running...", chat_id="4402961702", chat_type=ChatType.GROUP)
+# async def send_personal_message():
+#     dp = Dispatcher()
+#     client = Client(dp,session_file="session")
+#     async def open_gift_handler():
+#         try:
+#             await client.send_message(text=f"github action is running...", chat_id="4402961702", chat_type=ChatType.GROUP)
 
-            print("seccus")
-        except Exception as e:
-            print(f"Error {e}")
-    await open_gift_handler()
+#             print("seccus")
+#         except Exception as e:
+#             print(f"Error {e}")
+#     await open_gift_handler()
 
-asyncio.run(send_personal_message())
+# asyncio.run(send_personal_message())
+
+from pytube import YouTube
+YouTube('https://www.youtube.com/watch?v=OCGGoT23kh4').streams.first().download()
