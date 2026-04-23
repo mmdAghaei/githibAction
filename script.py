6fa159d8844c0f19f7526a25169e37354d6b4b92
Mohammad M. Aghaei <164787029+mmdAghaei@users.noreply.github.com>
2026-04-23 14:02:17 +0330

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,17 @@
-import datetime
+from aiobale import Client, Dispatcher
+import asyncio
+from aiobale.enums import ChatType
 
-print("Hello from GitHub Actions!")
-print(f"Current time is: {datetime.datetime.now()}")
+async def send_personal_message():
+    dp = Dispatcher()
+    client = Client(dp,session_file="session")
+    async def open_gift_handler():
+        try:
+            await client.send_message(text=f"github action is running...", chat_id="4402961702", chat_type=ChatType.GROUP)
+
+            print("seccus")
+        except Exception as e:
+            print(f"Error {e}")
+    await open_gift_handler()
+
+asyncio.run(send_personal_message())
