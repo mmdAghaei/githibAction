41d914f0e3314beb17f7fb1c836eb1ce5389f4c7
Mohammad M. Aghaei <mohammedaghaei401@gmail.com>
2026-04-24 22:35:43 +0330

    nn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import { fetchExtensionZip } from 'chrome-extension-fetch';
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
+const path = require('path');
+
 
 (async () => {
     try {
--- s.js
+++
@@ -1,4 +0,0 @@
-const axios = require('axios');
-const FormData = require('form-data');
-const fs = require('fs');
-const path = require('path');
