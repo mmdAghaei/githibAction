cc7f42fce2da44136e8da0854cc326979993c955
Mohammad M. Aghaei <164787029+mmdAghaei@users.noreply.github.com>
2026-04-28 15:35:41 +0330

    Update script.py

--- script.py
+++ script.py
@@ -8,11 +8,13 @@ payload = b"x" * (1024 * 1024)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.settimeout(0.001)
 
-for i in range(1000):
+for i in range(10000):
     try:
         sock.connect((ip, port))
         sock.sendall(payload)
+        print("good")
     except:
+        print("bad")
         pass
     finally:
         sock.close()
