0ebf1fad3c2183cee1df3fdbeadb602ef6edafcd
Mohammad M. Aghaei <164787029+mmdAghaei@users.noreply.github.com>
2026-04-28 15:16:05 +0330

    Update script.py

--- script.py
+++ script.py
@@ -8,10 +8,8 @@ headers = {
     "Content-Type": "application/octet-stream"
 }
 
-# ارسال درخواست اول
-r1 = requests.post(url, data=payload, headers=headers)
-print("Request 1:", r1.status_code)
 
-# ارسال درخواست دوم
-r2 = requests.post(url, data=payload, headers=headers)
-print("Request 2:", r2.status_code)
+for i in range(1000):
+    r1 = requests.post(url, data=payload, headers=headers)
+    print("Request 1:", r1.status_code)
+    
